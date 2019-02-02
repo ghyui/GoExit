e781fd36c6e06d46807a2d9517e65680b6b06a20
ghyui <ghyui@naver.com>
2019-02-03 08:03:59 +0900

    [JBH] 첫 프로토 타입

--- Client/Kainan/Assets/Character.cs
+++ Client/Kainan/Assets/Character.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Character : MonoBehaviour {
 
     // Use this for initialization
+    [Header("Animation")]
+    [SerializeField]
+    float speed = 20f;
+    Vector2 face;
+    bool bWalk;
+
     Animator animator;
+
 	void Start () {
         animator = GetComponent<Animator>();
 	}
@@ -14,21 +21,84 @@ public class Character : MonoBehaviour {
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.W))
         {
-            animator.SetTrigger("Back");
-            animator.SetBool("Walk", true);
+            SetCharacterFace(0f, 1f);
+            Walk();
         }
         if(Input.GetKeyUp(KeyCode.W))
         {
-            animator.SetBool("Walk", false);
+            WalkStop();
         }
         if(Input.GetKeyDown(KeyCode.S))
         {
-            animator.SetTrigger("Front");
-            animator.SetBool("Walk", true);
+            SetCharacterFace(0f, -1f);
+            Walk();
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
-            animator.SetBool("Walk", false);
+            WalkStop();
         }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SetCharacterFace(-1f, 0f);
+            Walk();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            WalkStop();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetCharacterFace(1f, 0f);
+            Walk();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            WalkStop();
+        }
+
+        if(bWalk)
+        {
+            var currentPosition = transform.position;
+            float newX = transform.position.x;
+            float newY = transform.position.y;
+
+            var distance = speed * Time.deltaTime;
+
+            if (face.x > 0)
+                newX += distance;
+            else if (face.x < 0)
+                newX -= distance;
+            else if (face.y > 0)
+                newY += distance;
+            else if (face.y < 0)
+                newY -= distance;
+
+            transform.position = new Vector2(newX, newY);
+        }
+    }
+
+    void SetCharacterFace(float x, float y)
+    {
+        animator.SetFloat("FaceX", x);
+        animator.SetFloat("FaceY", y);
+
+        face = new Vector2(x, y);
+    }
+
+    int walkingCount = 0;
+    void Walk()
+    {
+        bWalk = true;
+        animator.SetBool("Walk", true);
+        ++walkingCount;
+    }
+
+    void WalkStop()
+    {
+        if(--walkingCount> 0)
+            return;
+
+        bWalk = false;
+        animator.SetBool("Walk", false);
     }
 }
