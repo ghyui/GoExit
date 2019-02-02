fc1aa0d091ea45986ac129cb782b746a1ae9edf1
ghyui <ghyui@naver.com>
2019-02-02 12:50:11 +0900

    카이난 프로젝

---
+++ Client/Kainan/Assets/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    // Use this for initialization
+    Animator animator;
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Back");
+            animator.SetBool("Walk", true);
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Walk", false);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetTrigger("Front");
+            animator.SetBool("Walk", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Walk", false);
+        }
+    }
+}
